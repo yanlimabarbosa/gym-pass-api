7288220dfa96a3fcc6c8ccaeb3578239b42289ca
Yan Lima Barbosa <yanbr763@gmail.com>
2024-11-21 20:03:40 -0300

    feat: add Prisma ORM
    Adds Prisma ORM for database interactions.  Includes necessary dependencies and initial database interaction example.  This improves database management and simplifies data access.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Yan Lima Barbosa',
+    email: 'yanbr763@gmail.com',
+  },
+})
