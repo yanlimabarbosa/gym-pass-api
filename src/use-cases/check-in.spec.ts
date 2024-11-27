f1e97a7f5cbb805feb73c88655c15a3f93ae58b4
Yan Lima Barbosa <yanbr763@gmail.com>
2024-11-27 01:01:04 -0300

    feat: add check-in tests and in memory repository

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,23 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './checkin'
+import { randomUUID } from 'node:crypto'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Check-in Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: randomUUID(),
+      userId: randomUUID(),
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
