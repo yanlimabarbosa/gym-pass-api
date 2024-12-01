e2c419ada9c5aaa0629334682931dff185bbb67e
Yan Lima Barbosa <yanbr763@gmail.com>
2024-12-01 04:14:56 -0300

    feat(test): add create gym use case test

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      name: 'gym',
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
