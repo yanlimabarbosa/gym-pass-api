e5747457738d6b5310ec4eec74a3e33baec31c66
Yan Lima Barbosa <yanbr763@gmail.com>
2024-12-15 18:35:50 -0300

    feat: add tests for check-in validation use case

---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,42 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+import { randomUUID } from 'node:crypto'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate Check-in Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInsRepository)
+  })
+
+  afterEach(() => {
+    // vi.useRealTimers()
+  })
+
+  it('should be able to validate the check in', async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate an inexistente check in', async () => {
+    await expect(
+      sut.execute({ checkInId: 'inexistent-check-in-id' }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
